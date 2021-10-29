253988c0d519570e19d369ae7c79bc4a73ef6111
Amit Gajare <amitgajare2018@gmail.com>
2021-10-29 19:01:50 +0530

    product edit and delete

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -54,10 +54,10 @@
                                 <td>{{ $product->category->name }}</td>
                                 <td>{{ $product->created_at }}</td>
                                 <td>
-                                    <a href="{{ route('admin.editcategory', ['category_slug' => $product->slug]) }}">
+                                    <a href="{{ route('admin.editproduct', ['product_slug' => $product->slug]) }}">
                                         <i class="fa fa-edit fa-2x"></i>
                                     </a>
-                                    <a href="#" wire:click.prevent = "deleteCategory({{ $product->id }})" style="color:rgb(243, 24, 24); margin-left:10px;">
+                                    <a href="#" wire:click.prevent = "deleteProduct({{ $product->id }})" style="color:rgb(243, 24, 24); margin-left:10px;">
                                         <i class="fa fa-times fa-2x"></i>
                                     </a>
                                 </td>
